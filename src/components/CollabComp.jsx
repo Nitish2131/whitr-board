3e2d4b6068fb0d2fb6948ba9480187de23e4b781
nitish <nitish12@gmail.com>
2025-07-22 00:35:02 +0530

    collab mode done

--- src/components/CollabComp.jsx
+++ src/components/CollabComp.jsx
@@ -1,58 +1,92 @@
 import React from 'react'
-import { Tldraw, getSnapshot, useEditor } from 'tldraw'
+import { Tldraw, getSnapshot, useEditor, loadSnapshot } from 'tldraw'
 import 'tldraw/tldraw.css'
-import supabase from '../util/superbase';
-import { useParams } from 'react-router';
+import supabase from '../util/superbase'
+import { useParams } from 'react-router'
 
 function CollabComp() {
+    let { roomId } = useParams();
     function Toolbar() {
-        let { roomId } = useParams()
-        const editor = useEditor();
-        const save = async () => {
-            const Snapshot = getSnapshot(editor.store)
-        console.log('Room ID:', roomId)
-            
-            console.log(Snapshot);
-            alert('content saved')
-        
+      const editor = useEditor();
+  
+      async function checkIfKeyIsPresent() {
         try {
-
-            const { data, error } = await supabase
-                .from('white-boards')
-                .insert([
-                    { key:roomId,json:Snapshot },
-                ])
-                .select()
-
-                if(data){
-                    alert('save success')
-
-                }else{
-                    alert(error)
-                }
-
-
+            let { data , error } = await supabase
+            .from('whiteboards')
+            .select("*")
+            .eq("key", roomId);
+          if (data) {
+            return data;
+          }
+          return;
         } catch (err) {
-            console.log(err);
-
+          console.log(err);
         }
-    }
-
-        return (
-            <div style={{ pointerEvents: "all" }}>
-                <button onClick={save}>save data</button>
-
-            </div>
-        )
-    }
-
-    return (
-        <div style={{ position: 'fixed', inset: 0 }}>
-            <Tldraw components={{
-                SharePanel: () => <Toolbar />
-            }} />
+      }
+  
+      async function save() {
+        const snapshot = getSnapshot(editor.store);
+        try {
+          const prevData = await checkIfKeyIsPresent();
+          console.log(prevData)
+          if (prevData && prevData.length > 0) {
+            const { data, error } = await supabase
+              .from("whiteboards")
+              .update({ json: JSON.stringify(snapshot) })
+              .eq('key', roomId)
+              .select();
+            if (data) {
+              alert("Update Success");
+            }
+            return;
+          }
+          const { data, error } = await supabase
+            .from("whiteboards")
+            .insert([{ key: roomId, json: JSON.stringify(snapshot) }])
+            .select();
+          if (data) {
+            alert("Save Success");
+          } else {
+            alert(error);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      return (
+        <div style={{ pointerEvents: "all" }}>
+          <button onClick={save}>Save Data</button>
         </div>
-
-    )
-}
-export default CollabComp
+      );
+    }
+  
+    async function loadData(editor) {
+      try {
+        let { data, error } = await supabase
+          .from("whiteboards")
+          .select("*")
+          .eq("key", roomId);
+        if (data && data[0]) {
+          console.log(data);
+          loadSnapshot(editor.store, JSON.parse(data[0]?.json));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    return (
+      <div style={{ position: "fixed", inset: 0 }}>
+        <Tldraw
+          components={{
+            SharePanel: () => <Toolbar />,
+          }}
+          onMount={(editor) => {
+            loadData(editor);
+          }}
+        />
+      </div>
+    );
+  }
+  
+  export default CollabComp;
+  
